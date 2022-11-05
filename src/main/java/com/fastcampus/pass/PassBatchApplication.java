c9eae43c5f75373ed10f49d5d6587d1b8beff1c2
EunjoungAhn <aej9300@gmail.com>
2022-11-05 23:50:02 +0900

    git에 Batch프로젝트 구현

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,10 +1,53 @@
 package com.fastcampus.pass;
 
+import net.bytebuddy.utility.nullability.AlwaysNull;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
+
+	//Batch를 만들기 위해서 = Job을 만들기 위해서 Step을 만들어 주어야 한다.
+	// 그 Step 으로 Job을 구성해주어야 한다.
+
+	private final JobBuilderFactory jobBuilderFactory;
+
+	private final StepBuilderFactory stepBuilderFactory;
+
+	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	//스텝 선언
+	@Bean
+	public Step passStep(){
+		return this.stepBuilderFactory.get("passStep")
+				.tasklet((contribution, chunkContext) -> {
+						System.out.println("Execute PassStep");
+						return RepeatStatus.FINISHED;
+				}).build();
+
+	}
+
+	@Bean
+	public Job passJob(){
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
